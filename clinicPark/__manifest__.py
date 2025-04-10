266238ffb5080c266b5d762c5ae3022f309c8905
juanangel89 <juansrangel89@gmail.com>
2025-04-10 15:07:28 -0500

    subir cambio a main de patient5

--- clinicPark/__manifest__.py
+++ clinicPark/__manifest__.py
@@ -1,11 +1,9 @@
 {
     'name': 'Clinic Park - Gestión de Pacientes',
     'version': '1.0',
-    'summary': 'Módulo de gestión de pacientes para la Clínica del Parque',
-    'description': 'Permite registrar pacientes, edad, género y motivo de ingreso.',
-    'author': 'juanangel89',
-    'category': 'Healthcare',
-    'depends': ['base'],
+    'depends': ['base'
+                
+    ],
     'data': [
         'security/ir.model.access.csv',
         'views/patient_views.xml',
